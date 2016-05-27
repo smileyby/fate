478f050d6e1f74eb01e20a26449d9d9d3c25606c
smileyby <983332827@qq.com>
2016-05-27 19:56:44 +0800

    fate

---
+++ js/fate.js
@@ -0,0 +1,28 @@
+$(function(){
+	var index = 0;
+	var t = setInterval(function(){
+		// if(index == 0){
+		// 	console.log(0);
+		// 	$(".bannerIMG").eq(index).animate({"left":"-404px","zIndex":"1"});
+		// 	$(".bannerIMG").eq(index+1).animate({"left":"0","zIndex":"10"});
+		// 	$(".bannerIMG").eq(index+2).animate({"left":"-404px","zIndex":"1"});
+		// }
+		// if(index == 1){
+		// 	console.log(1);
+		// 	$(".bannerIMG").eq(index-1).animate({"left":"404px","zIndex":"1"});
+		// 	$(".bannerIMG").eq(index).animate({"left":"-404","zIndex":"1"});
+		// 	$(".bannerIMG").eq(index+1).animate({"left":"0","zIndex":"10"});
+		// }
+		// if(index == 2){
+		// 	console.log(2);
+		// 	$(".bannerIMG").eq(index-2).animate({"left":"0","zIndex":"10"});
+		// 	$(".bannerIMG").eq(index-1).animate({"left":"-404px","zIndex":"1"});
+		// 	$(".bannerIMG").eq(index).animate({"left":"404px","zIndex":"1"});
+		// }
+		// index += 1;
+		// if(index > 2){
+		// 	index = 0;
+		// }
+
+	},3000);
+})
